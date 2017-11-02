77b04bf96b78d554316b73b14fa3663c3027245c
sasakidmap <sasaki@dmap.jp>
2017-11-02 17:14:49 +0900

    [WIP] setup.py check

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='glacierclient',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['termcolor'],
+    entry_points={
+        'console_scripts': 'glacierclient = glacierclient.main:main'
+    },
+    data_files=[('/usr/local/etc', ['glacier.conf'])],
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
