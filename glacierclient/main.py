bd79e1ac3b8b7e52f931c638681a9dcbdde38c3a
sasakidmap <sasaki@dmap.jp>
2017-11-02 17:22:21 +0900

    [WIP] logger

--- glacierclient/main.py
+++ glacierclient/main.py
@@ -4,13 +4,16 @@ main function
 """
 
 from glacierclient.glacier import Glacier
+from glacierclient import logger
 
 def main():
     """
     main function
     """
+    logger.debug('--- main start ---')
     glacier = Glacier()
     glacier.create_vault()
+    logger.debug('--- main  end  ---')
 
 if __name__ == "__main__":
     main()
